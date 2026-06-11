9c3188d219e973f5b0a9d06b6af57f929aaac22f
Sadhana Chouhan <chouhansadhana17@gmail.com>
2026-06-11 15:15:52 +0530

    feat: implement user service

--- Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System/src/main/java/com/payment/ledger/repository/UserRepository.java
+++ Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System/src/main/java/com/payment/ledger/repository/UserRepository.java
@@ -1,11 +1,13 @@
-package com.wallet.repository;
+package com.payment.ledger.repository;
 
-import com.wallet.model.User;
+import com.payment.ledger.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByEmail(String email);
