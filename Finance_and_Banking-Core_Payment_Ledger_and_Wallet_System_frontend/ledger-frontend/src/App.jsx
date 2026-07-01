0bcee9953279c68faec92ba3536a9597cf5cebac
janagamadeepika23 <janagamadeepika23@gmail.com>
2026-07-01 17:24:16 +0530

    Implement TransferPage with transfer form and idempotency support

--- Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System_frontend/ledger-frontend/src/App.jsx
+++ Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System_frontend/ledger-frontend/src/App.jsx
@@ -1,20 +1,9 @@
-import React from 'react'
-import RegisterPage from './pages/RegisterPage.jsx'
-
-const App = () => {
-  return (
-    <div>App
-      <RegisterPage/>
-    </div>
-  )
-}
-
-export default App
-
-//we need follow these method deepika
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import DashboardPage from "./pages/DashboardPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import TransferPage from "./pages/TransferPage.jsx";
+
 
 function App() {
   return (
@@ -23,6 +12,8 @@ function App() {
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
          <Route path="/dashboard" element={<DashboardPage />} />
+         <Route path="/register" element={<RegisterPage />} />
+          <Route path="/transfer" element={<TransferPage />} />
       </Routes>
     </BrowserRouter>
   );
