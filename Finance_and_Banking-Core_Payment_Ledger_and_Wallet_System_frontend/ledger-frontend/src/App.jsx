c1007281cf87f9073d9c4fb244ac7689925e57be
Meet Prajapati <159464771+prajapati-meet@users.noreply.github.com>
2026-07-03 10:53:40 +0530

    fix :Add React import to App.jsx

--- Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System_frontend/ledger-frontend/src/App.jsx
+++ Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System_frontend/ledger-frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage.jsx";
 import DashboardPage from "./pages/DashboardPage.jsx";
