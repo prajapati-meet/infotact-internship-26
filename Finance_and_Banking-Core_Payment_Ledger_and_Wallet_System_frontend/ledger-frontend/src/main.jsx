2d10142e9fd756fed6d6159c537b0d2a039e68b1
janagamadeepika23 <janagamadeepika23@gmail.com>
2026-07-06 14:58:30 +0530

    Update App, main, and Navbar components

--- Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System_frontend/ledger-frontend/src/main.jsx
+++ Finance_and_Banking-Core_Payment_Ledger_and_Wallet_System_frontend/ledger-frontend/src/main.jsx
@@ -1,23 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-//we need follow these method deepika
-createRoot(document.getElementById('root')).render(
- <BrowserRouter>
- <StrictMode>
-    <App />
-  </StrictMode>,
-  </BrowserRouter>
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
-
-
-
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
